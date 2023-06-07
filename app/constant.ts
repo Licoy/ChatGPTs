cdb4dcf4c20187c0524c1a3a184de9ea8f493ba4
licoy <licoycn@gmail.com>
2023-06-07 14:47:21 +0800

    更改链接为本项目的github地址以提供支持

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "Licoy";
+export const REPO = "ChatGPT-Midjourney";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
