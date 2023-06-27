915fd7a7deab131f7df76721e12d979e86a3bd7e
licoy <licoycn@gmail.com>
2023-06-27 22:32:12 +0800

    feat: resize mobile screen button size

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getBuildConfig } from "./config/build";
+import BgPng from "./icons/bg.png";
 
 const buildConfig = getBuildConfig();
 
